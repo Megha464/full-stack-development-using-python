01a9637469debe2e939a3cfaf386d7966e9274ae
Megha464 <ameghana326@gmail.com>
2026-07-07 15:44:07 +0530

    Add files via upload
    day2

---
+++ operators.py
@@ -0,0 +1,193 @@
+# product_price=5000
+# delivery_charge=100
+# total_cost= product_price + delivery_charge
+# print(total_cost)
+
+# ####arthimatic operator########
+# a=10
+# b=3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+# ################
+# student=10
+# group=2
+# print(student//group)
+# ###########assignement operator##############
+
+# followers=1000
+# followers += 1
+# print(followers)
+
+# ############### comparision oparetor####
+# saved_passweord="1234"
+# entered_password="1234"
+# print(saved_passweord == entered_password)
+
+# =
+# ==
+# <
+# >
+# >=
+# <=
+
+# balance=500
+# pin_correct=True
+# if balance >= 1000 and pin_correct:
+#     print("withdraw allowed")
+# else:
+#     print("failed")
+
+######calculatorbill####
+# product_price = int(input("Enter product price: "))
+# delivery_charge = int(input("Enter delivery charge: "))
+# discount_percent = float(input("Enter discount percentage: "))
+
+# discount_amount = (product_price * discount_percent) / 100
+
+# total_bill = product_price + delivery_charge - discount_amount
+
+# print("\n------ BILL ------")
+# print("Product Price    :", product_price)
+# print("Delivery Charge  :", delivery_charge)
+# print("Discount (%)     :", discount_percent)
+# print("Discount Amount  :", discount_amount)
+# print("------------------")
+# print("Total Bill       :", total_bill)
+
+######password#####
+# password= input("enter the password")
+# if password =="admin1234":
+#     print("welcome")
+# else:
+#     print("worng password")
+
+ #####conditon#####
+# age = 30
+# if age>=18:
+#     print("eligible for voting")
+# else:
+#     print("not eligible for voting")
+
+##### marks#######
+# marks = 85
+# if marks>=90:
+#     print("grade A")
+# elif marks>=75:
+#     print("grade B")
+# elif marks>=50:
+#     print("grade C")
+# else:
+#     print("fail")    
+
+
+# marks = 65
+# if marks>=90:
+#     print("cgpa 9")
+# elif marks>=75:
+#     print("cgpa 8")
+# elif marks>=50:
+#     print("cgpa 6")
+# else:
+#     print("fail")   
+
+#####logical operator####
+# age =25
+# salary =50000
+# if age >=18 and salary>=30000:
+#     print("loan approve")
+
+###### or gate ###
+#day ="sunday"
+#if day =="saturday"or "sunday":
+ #   print("holiday")
+
+# login =True
+# if not login:
+#     print("login failed")
+
+
+# pin = int(input("Enter your pin: "))
+# bank_balance = 10000
+
+# if pin == 1234:
+#     print("Correct pin")
+#     amount = int(input("Enter amount to withdraw: "))
+#     if amount <= bank_balance:
+#         print("Withdrawal successful")
+# else:
+#     print("Wrong pin")
+
+
+# pin = int(input("Enter your pin: "))
+# bank_balance = 10000
+
+# if pin == 1234:
+#     print("Correct pin")
+#     amount = int(input("Enter amount to withdraw: "))
+#     if amount <= bank_balance:
+#         print("Withdrawal successful")
+#         print("Remaining balance:", bank_balance - amount)
+# else:
+#     print("Wrong pin")
+
+# send mail
+# print("send mail")
+
+# for i in range(5):
+# print("send mail")
+
+
+# users =["mahi","megha","nandhu"]
+# for users in users:
+#     print("message sent to",users)
+
+
+# for i in range(2,6):
+#         print(i)
+
+# name = "megha"
+# for ch in name:
+#     print(ch)
+
+# #####while loop####
+# i=0
+# while i<=5:
+#     print("send mail")
+#     i+=1
+
+# for i in range (10):
+#  if i ==5:
+#   break
+#  print(i)
+
+# for i in range(10):
+#     if i==5:
+#         continue
+# print(i)
+
+# password = ""
+# while password != "admin123":
+#    password = input("Enter your password: ")
+#    print("login success")
+
+# student1 ="ram"
+# student2 ="sam"
+# student3 ="nithin"
+
+# student =[
+#     "ram",
+#     "sam",
+#     "rana"
+# ]
+# student.append("alex")
+# student.remove("sam")
+# print(student)
+
+
+    
